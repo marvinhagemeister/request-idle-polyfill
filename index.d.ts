e85745dbadb07eeb69b0ee783361c1c0ba21e409
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-12-23 19:26:18 +0100

    Add missing typings when using with jsdom

--- index.d.ts
+++ index.d.ts
@@ -7,3 +7,9 @@ interface Window {
   requestIdleCallback(cb: (deadline: RequestIdleCallback) => any): NodeJS.Timer;
   cancelIdleCallback(id: NodeJS.Timer): void;
 }
+
+declare namespace NodeJS {
+  interface Global {
+    window?: Window;
+  }
+}
